617dd99a3d56f9b37e97ac8b8c0b431b70e75512
emborromeo <ericaborromeo06@gmail.com>
2020-07-12 20:41:28 -0700

    new changes

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-document.write("Hello World");
+
+function view(){
+    let content= document.getElementById("text-notes").value;
+    document.getElementById("a1").innerHTML=content;
+    document.getElementById("text-notes").value=" ";
+}
+function clear(){
+    document.getElementById("text-notes").innerText=" ";
+}
