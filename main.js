3c372074d2259de7d7e20c0fb30964cabcc46fe8
emborromeo <ericaborromeo06@gmail.com>
2020-07-13 01:20:29 -0700

    changes made

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 
 function view(){
+
     let content= document.getElementById("text-notes").value;
-    document.getElementById("a1").innerHTML=content;
+    for(i=0; i<10;i++){
+    document.getElementById("1").innerHTML=content;
     document.getElementById("text-notes").value=" ";
+    document.write(i);
+}
 }
 function clear(){
     document.getElementById("text-notes").innerText=" ";
